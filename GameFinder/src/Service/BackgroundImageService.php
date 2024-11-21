36f3ddeb349b25de8db846764490b4d7d0918a2e
Jordan <jordan.cabrera.t23@gmail.com>
2024-11-21 11:06:34 +0000

    Proyect Finished

--- GameFinder/src/Service/BackgroundImageService.php
+++ GameFinder/src/Service/BackgroundImageService.php
@@ -13,7 +13,9 @@ class BackgroundImageService extends AbstractController
         'image/2.jpg',
         'image/3.jpg',
         'image/4.jpg',
-        'image/5.jpg'
+        'image/5.jpg',
+        'image/6.jpg',
+        'image/7.jpg'
     ];
 
     public function __construct()
